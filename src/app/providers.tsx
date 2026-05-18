205b7ac82bb15cf082d93bf1c6bba2099d072f39
sahban <sahbaan@proton.me>
2026-05-18 16:44:33 +0530

    feat: consolidate WagmiProvider logic into Providers component and remove unused file

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,56 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  WagmiProvider,
+  http,
+  cookieStorage,
+  createStorage,
+  createConfig,
+} from "wagmi";
+import { base } from "wagmi/chains";
+import { type ReactNode, useState, useEffect } from "react";
+import { injected } from "wagmi/connectors";
+import sdk from "@farcaster/miniapp-sdk";
 
-const WagmiProvider = dynamic(
-  () => import("@/components/providers/WagmiProvider"),
-  {
-    ssr: false,
-  }
-);
+export const config = createConfig({
+  chains: [base],
+  connectors: [injected()],
+  transports: {
+    [base.id]: http(),
+  },
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+});
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <WagmiProvider>{children}</WagmiProvider>;
+export function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  useEffect(() => {
+    let isMounted = true;
+    const load = async () => {
+      await sdk.context;
+      if (isMounted) {
+        sdk.actions.ready({ disableNativeGestures: true });
+      }
+    };
+
+    if (sdk) {
+      load();
+    }
+
+    return () => {
+      isMounted = false;
+      if (sdk) {
+        sdk.removeAllListeners();
+      }
+    };
+  }, []);
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
 }
