678db10d658a421fcd9b0da95488760290b81758
sahban <sahbaan@proton.me>
2026-05-18 16:45:10 +0530

    feat: replace Main component with YearProgress and remove unused Main file

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,10 +2,16 @@
 
 import dynamic from "next/dynamic";
 
-const Main = dynamic(() => import("@/components/Main"), {
+const YearProgress = dynamic(() => import("@/components/YearProgress"), {
   ssr: false,
 });
 
 export default function App() {
-  return <Main />;
+  return (
+    <div className="min-h-screen flex flex-col justify-between">
+      <main className="grow w-full flex flex-col flex-1 min-h-0 bg-linear-to-b from-slate-900 to-slate-800">
+        <YearProgress />
+      </main>
+    </div>
+  );
 }
